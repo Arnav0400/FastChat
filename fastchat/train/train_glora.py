335542bf8d0b97690a3fc71c1890c4395380eb86
Arnav Chavan <44640932+Arnav0400@users.noreply.github.com>
2023-06-30 14:52:48 +0530

    added glora training

---
+++ fastchat/train/train_glora.py
@@ -0,0 +1,162 @@
+# Usage: deepspeed train_lora.py --deepspeed <$PATH_TO_DEEPSPEED_CONFIG>
+
+# Adopted from tatsu-lab@stanford_alpaca. Below is the original copyright:
+#    Copyright 2023 Rohan Taori, Ishaan Gulrajani, Tianyi Zhang, Yann Dubois, Xuechen Li
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from dataclasses import dataclass, field
+import logging
+import pathlib
+import typing
+import os
+
+from deepspeed import zero
+from deepspeed.runtime.zero.partition_parameters import ZeroParamStatus
+import sys
+sys.path.append('/nfs/users/ext_arnav.chavan/NIPS23/peft/src')
+from peft import GLoraConfig, get_peft_model, prepare_model_for_kbit_training
+import transformers
+from transformers import Trainer, BitsAndBytesConfig, deepspeed
+import torch
+
+from fastchat.train.train import (
+    DataArguments,
+    ModelArguments,
+    TrainingArguments,
+    make_supervised_data_module,
+)
+
+# from fastchat.train.llama_flash_attn_monkey_patch import (
+#     replace_llama_attn_with_flash_attn,
+# )
+
+# replace_llama_attn_with_flash_attn()
+
+
+@dataclass
+class GLoraArguments:
+    glora_r: int = 4
+    glora_target_modules: typing.List[str] = field(
+        default_factory=lambda: ["q_proj"]
+    )
+    glora_weight_path: str = ""
+
+
+def maybe_zero_3(param):
+    if hasattr(param, "ds_id"):
+        assert param.ds_status == ZeroParamStatus.NOT_AVAILABLE
+        with zero.GatheredParameters([param]):
+            param = param.data.detach().cpu().clone()
+    else:
+        param = param.detach().cpu().clone()
+    return param
+
+
+# Borrowed from peft.utils.get_peft_model_state_dict
+def get_peft_state_maybe_zero_3(named_params):
+    to_return = {k: t for k, t in named_params if "glora_" in k}
+    to_return = {k: maybe_zero_3(v) for k, v in to_return.items()}
+    return to_return
+
+
+def train():
+    parser = transformers.HfArgumentParser(
+        (ModelArguments, DataArguments, TrainingArguments, GLoraArguments)
+    )
+    (
+        model_args,
+        data_args,
+        training_args,
+        glora_args,
+    ) = parser.parse_args_into_dataclasses()
+
+    device_map = None
+
+    compute_dtype = (
+        torch.float16
+        if training_args.fp16
+        else (torch.bfloat16 if training_args.bf16 else torch.float32)
+    )
+
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_args.model_name_or_path,
+        cache_dir=training_args.cache_dir,
+        device_map=device_map,
+    )
+    glora_config = GLoraConfig(
+        r=glora_args.glora_r,
+        target_modules=glora_args.glora_target_modules,
+        task_type="CAUSAL_LM",
+    )
+
+    # if lora_args.q_lora:
+    #     model = prepare_model_for_kbit_training(
+    #         model, use_gradient_checkpointing=training_args.gradient_checkpointing
+    #     )
+    #     if torch.cuda.device_count() > 1:
+    #         # keeps Trainer from trying its own DataParallelism when more than 1 gpu is available
+    #         model.is_parallelizable = True
+    #         model.model_parallel = True
+
+    model = get_peft_model(model, glora_config)
+    if training_args.deepspeed is not None and training_args.local_rank == 0:
+        model.print_trainable_parameters()
+
+    if training_args.gradient_checkpointing:
+        model.enable_input_require_grads()
+
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        model_args.model_name_or_path,
+        cache_dir=training_args.cache_dir,
+        model_max_length=training_args.model_max_length,
+        padding_side="right",
+        use_fast=False,
+    )
+    tokenizer.pad_token = tokenizer.unk_token
+
+    data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
+    trainer = Trainer(
+        model=model, tokenizer=tokenizer, args=training_args, **data_module
+    )
+
+    model.config.use_cache = False
+
+    if list(pathlib.Path(training_args.output_dir).glob("checkpoint-*")):
+        trainer.train(resume_from_checkpoint=True)
+    else:
+        trainer.train()
+    trainer.save_state()
+
+    # check if zero3 mode enabled
+    if trainer.hf_deepspeed_config_orig.is_zero3():
+        # use deepspeed engine internal function to gather state dict
+        # state_dict_zero3 contains whole parameters of base and lora adapters
+        # we will not extract lora parameters since peft save_pretrained will do that
+        # https://github.com/huggingface/peft/blob/3714aa2fff158fdfa637b2b65952580801d890b2/src/peft/peft_model.py#L125
+        # https://github.com/huggingface/peft/blob/3714aa2fff158fdfa637b2b65952580801d890b2/src/peft/utils/save_and_load.py#L19
+        state_dict_zero3 = trainer.model_wrapped._zero3_consolidated_16bit_state_dict()
+        if training_args.local_rank == 0:
+            state_dict = state_dict_zero3
+    else:
+        # in other mode we use original code from fastchat team, to make sure our change is minimum
+        state_dict = get_peft_state_maybe_zero_3(
+            model.named_parameters()
+        )
+
+    if training_args.local_rank == 0:
+        model.save_pretrained(training_args.output_dir, state_dict=state_dict)
+
+
+if __name__ == "__main__":
+    train()
